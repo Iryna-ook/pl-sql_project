c761ec89ddbe9a5c620f763063d2df00d0cb8ada
Iryna <malookirina25@gmail.com>
2024-07-28 21:29:30 +0300

    Add files via upload

---
+++ 3_table_emploeeys_history.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE employees_history (employee_id number(6),
+                                first_name varchar2(20),
+                                last_name varchar2(25),
+                                email varchar2(25),
+                                phone_number varchar2(20),
+                                hire_date date,
+                                job_id varchar2(10),
+                                salary number(8,2),
+                                commission_pct number(2,2),
+                                manager_id number(6),
+                                department_id number(4),
+                                fire_date date);    
+               
